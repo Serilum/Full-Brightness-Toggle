8a90678a0e707abaf264a5fc47a7f081d2fd4973
Rick South <rick@natamus.com>
2024-01-01 22:17:02 +0100

    (v4.0) The fullbright state is now persistent across game launches. Saved in: ./config/fullbrightnesstoggle/laststate.txt

--- Common/src/main/java/com/natamus/fullbrightnesstoggle/util/Reference.java
+++ Common/src/main/java/com/natamus/fullbrightnesstoggle/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.fullbrightnesstoggle.util;
 public class Reference {
 	public static final String MOD_ID = "fullbrightnesstoggle";
 	public static final String NAME = "Full Brightness Toggle";
-	public static final String VERSION = "3.3";
+	public static final String VERSION = "4.0";
 	public static final String ACCEPTED_VERSIONS = "[1.20.4]";
 }
