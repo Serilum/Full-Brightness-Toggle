dca74828ce4591983f540f1c7e605a0556133989
Rick South <rick@natamus.com>
2024-05-01 16:40:11 +0200

    Full Brightness Toggle source code for 1.20.6.

---
+++ Fabric/src/main/java/com/natamus/fullbrightnesstoggle/ModFabricClient.java
@@ -0,0 +1,23 @@
+package com.natamus.fullbrightnesstoggle;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import com.natamus.fullbrightnesstoggle.data.Constants;
+import com.natamus.fullbrightnesstoggle.events.ToggleEvent;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.KeyMapping;
+
+public class ModFabricClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		Constants.hotkey = KeyBindingHelper.registerKeyBinding(new KeyMapping("fullbrightnesstoggle.key.togglebrightness", InputConstants.Type.KEYSYM, 71, "key.categories.misc"));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (Constants.hotkey.isDown()) {
+				ToggleEvent.onHotkeyPress();
+				Constants.hotkey.setDown(false);
+			}
+		});  	
+	}
+}
