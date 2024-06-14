fc61313f9b42e0c3abe1fde6cab6f2f064c3ee02
Rick South <rick@natamus.com>
2024-06-14 05:30:44 +0200

    Full Brightness Toggle source code for 1.21.0.

---
+++ NeoForge/src/main/java/com/natamus/fullbrightnesstoggle/neoforge/events/NeoForgeKeyMappingRegister.java
@@ -0,0 +1,17 @@
+package com.natamus.fullbrightnesstoggle.neoforge.events;
+
+import com.natamus.fullbrightnesstoggle.data.Constants;
+import net.minecraft.client.KeyMapping;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber(value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
+public class NeoForgeKeyMappingRegister {
+	@SubscribeEvent
+	public static void registerKeyBinding(RegisterKeyMappingsEvent e) {
+		Constants.hotkey = new KeyMapping("fullbrightnesstoggle.key.togglebrightness", 71, "key.categories.misc");
+		e.register(Constants.hotkey);
+	}
+}
