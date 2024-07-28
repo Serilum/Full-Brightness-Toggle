2368c683fbc9bce508e62fd15b2662fdc121a9be
Rick South <rick@natamus.com>
2024-07-28 07:32:07 +0200

    (v4.2) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/fullbrightnesstoggle/util/Reference.java
+++ Common/src/main/java/com/natamus/fullbrightnesstoggle/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.fullbrightnesstoggle.util;
 public class Reference {
 	public static final String MOD_ID = "fullbrightnesstoggle";
 	public static final String NAME = "Full Brightness Toggle";
-	public static final String VERSION = "4.1";
+	public static final String VERSION = "4.2";
 	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
 }
--- Fabric/src/main/java/com/natamus/fullbrightnesstoggle/ModFabricClient.java
+++ Fabric/src/main/java/com/natamus/fullbrightnesstoggle/ModFabricClient.java
@@ -4,6 +4,8 @@ import com.mojang.blaze3d.platform.InputConstants;
 import com.natamus.fullbrightnesstoggle.data.Constants;
 import com.natamus.fullbrightnesstoggle.events.ToggleEvent;
 import net.fabricmc.api.ClientModInitializer;
+import com.natamus.fullbrightnesstoggle.util.Reference;
+import com.natamus.collective.check.ShouldLoadCheck;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
@@ -11,6 +13,10 @@ import net.minecraft.client.KeyMapping;
 public class ModFabricClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		Constants.hotkey = KeyBindingHelper.registerKeyBinding(new KeyMapping("fullbrightnesstoggle.key.togglebrightness", InputConstants.Type.KEYSYM, 71, "key.categories.misc"));
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
