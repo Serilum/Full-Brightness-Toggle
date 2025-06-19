7c80675888497ec6c0de0bd6c4cbeadb0240d5ed
Rick South <rick@natamus.com>
2025-06-19 03:53:25 +0200

    Full Brightness Toggle source code for 1.21.6.

---
+++ Forge/src/main/java/com/natamus/fullbrightnesstoggle/ModForge.java
@@ -0,0 +1,47 @@
+package com.natamus.fullbrightnesstoggle;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.fullbrightnesstoggle.forge.events.ForgeKeyMappingRegister;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import com.natamus.fullbrightnesstoggle.forge.events.ForgeToggleEvent;
+import com.natamus.fullbrightnesstoggle.util.Reference;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge(FMLJavaModLoadingContext modLoadingContext) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		if (!FMLEnvironment.dist.equals(Dist.CLIENT)) {
+			return;
+		}
+
+		BusGroup busGroup = modLoadingContext.getModBusGroup();
+
+		FMLLoadCompleteEvent.getBus(busGroup).addListener(this::loadComplete);
+		RegisterKeyMappingsEvent.getBus(busGroup).addListener(ForgeKeyMappingRegister::registerKeyBinding);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+    	ForgeToggleEvent.registerEventsInBus();
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
