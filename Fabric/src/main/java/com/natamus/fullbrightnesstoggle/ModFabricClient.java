bba8fee7b765ef1424b8874e1d778b29fdff9a4b
Rick South <rick@natamus.com>
2025-10-03 01:14:58 +0200

    (v4.4) Updated mod to use Collective's new RegisterKeyMappingsHelper.

--- Fabric/src/main/java/com/natamus/fullbrightnesstoggle/ModFabricClient.java
+++ Fabric/src/main/java/com/natamus/fullbrightnesstoggle/ModFabricClient.java
@@ -1,14 +1,11 @@
 package com.natamus.fullbrightnesstoggle;
 
-import com.mojang.blaze3d.platform.InputConstants;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.fullbrightnesstoggle.data.Constants;
 import com.natamus.fullbrightnesstoggle.events.ToggleEvent;
-import net.fabricmc.api.ClientModInitializer;
 import com.natamus.fullbrightnesstoggle.util.Reference;
-import com.natamus.collective.check.ShouldLoadCheck;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.KeyMapping;
 
 public class ModFabricClient implements ClientModInitializer {
 	@Override
@@ -17,7 +14,7 @@ public class ModFabricClient implements ClientModInitializer {
 			return;
 		}
 
-		Constants.hotkey = KeyBindingHelper.registerKeyBinding(new KeyMapping("fullbrightnesstoggle.key.togglebrightness", InputConstants.Type.KEYSYM, 71, "key.categories.misc"));
+		ModCommon.registerHotkeys();
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			while (Constants.hotkey.isDown()) {
